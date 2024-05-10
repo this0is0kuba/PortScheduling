e6ae280688f4561c47358df2e55dbcb1a9c5e21e
this0is0kuba <jakub.rakus06@gmail.com>
2024-05-10 09:44:49 +0200

    fix partial solver

--- solvers/partial.py
+++ solvers/partial.py
@@ -1,9 +1,10 @@
 from minizinc import Instance, Model, Solver
 from datetime import timedelta
+import math
 
 class PartialAlgorithm:
 
-    def __init__(self, ship_size: list[int], arrival: list[int], maxtime: int, number_of_parts: int):
+    def __init__(self, ship_size: list[int], arrival: list[int], maxtime: int, number_of_parts: int, additional_time: int, stop_after: int):
 
         self.nsh = len(ship_size)
         self.ship_size = ship_size
@@ -30,18 +31,157 @@ class PartialAlgorithm:
         self.reclaimers_number = [2, 2, 2, 3, 3, 7, 7, 10, 1, 1, 2, 1]
         self.reclaimers_platform = [1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7,
                                     7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 10, 11, 11, 12]
+        
+        self.additional_time = additional_time
+        self.time_part = math.ceil( (maxtime - self.additional_time + 1) / self.number_of_parts)
+        self.earlier_solution = None
+        self.stop_after = stop_after
+        
+        # solution
+        self.which = []
+        self.ship_position = []
+        self.start = []
+        self.obj = 0
 
     def solve(self):
 
-        model = Model("portSchedulingPartial.mzn")
-        solver = Solver.lookup("com.google.ortools.sat")
-        instance = Instance(solver, model)
+        print("ship_size: ", self.ship_size)
+        print("arrival: ", self.arrival)
+        print()
+        
+        for i in range(self.number_of_parts):
 
-        instance["nsh"] = self.nsh
-        instance["ship_size"] = self.ship_size
-        instance["load_goods"] = self.load_goods
-        instance["maxtime"] = self.maxtime
-        instance["arrival"] = self.arrival
+            if i == 0:
+                self.solve_using_standard_method(i)
+
+            else: 
+
+                if self.earlier_solution == None:
+                    self.solve_using_standard_method(i)
+                
+                else:
+                    idx_range = self.find_earlier_range(self.earlier_solution['left'])
+
+                    if len(idx_range) == 0:
+                        self.solve_using_standard_method(i)
+                    else:
+                        self.solve_using_partial_method(i)
+
+        print()
+        print()
+
+        return {
+            "which": self.which,
+            "ship_position": self.ship_position,
+            "start": self.start,
+            "obj": self.obj
+        }
+
+
+    def solve_using_standard_method(self, i):
+            
+            print("\n\n--------------------- part number: ", i , " (standard method) ", f" time = [{self.time_part * i}, {(self.time_part) * (1 + i) - 1}]", " ---------------------")
+        
+            min_index, max_index = self.find_range(i)
+
+            if min_index > max_index:
+
+                self.earlier_solution = None
+                return
+
+            model = Model("portScheduling2.mzn")
+            solver = Solver.lookup("com.google.ortools.sat")
+            instance = Instance(solver, model)
+
+            self.set_initial_values(instance, i)
+
+            max_time = timedelta(seconds=60*self.stop_after/self.number_of_parts)
+            result = instance.solve(timeout=max_time, processes=8)
+
+            dict_result = {
+                "which": result["which"],
+                "ship_position": result["ship_position"],
+                "start": result["start"],
+                "left": result["left"],
+                "reclaimers_position": result["reclaimers_position"],
+                "obj": result["objective"],
+                "min_index": min_index,
+                "max_index": max_index
+            }
+
+            print("ship_indexes: ", [x for x in range(min_index, max_index+1)])
+            print("ship_size: ", self.ship_size[min_index:max_index+1])
+            print("which: ", result["which"])
+            print("start: ", [time + i * self.time_part for time in result["start"]])
+            print("left: ", result["left"])
+            print("obj: ", result["objective"])
+            print("min index: ", min_index)
+            print("max_index: ", max_index)
+
+            self.which.extend(result["which"])
+            self.ship_position.extend(result["ship_position"])
+            self.start.extend([time + i * self.time_part for time in result["start"]])
+            self.obj += result["objective"]
+
+            self.earlier_solution = dict_result
+
+    def solve_using_partial_method(self, i):
+            
+            print("\n\n--------------------- part number: ", i , " (partial method) ", f" time = [{self.time_part * i}, {(self.time_part) * (1 + i) - 1}]", " ---------------------")
+            
+            min_index, max_index = self.find_range(i)
+
+            if min_index > max_index:
+
+                self.earlier_solution = None
+                return
+
+            model = Model("portSchedulingPartial.mzn")
+            solver = Solver.lookup("com.google.ortools.sat")
+            instance = Instance(solver, model)
+
+            self.set_initial_values(instance, i)
+            self.set_earlier_values(instance, i)
+
+            max_time = timedelta(seconds=60*self.stop_after/self.number_of_parts)
+            result = instance.solve(timeout=max_time, processes=8)
+
+            dict_result = {
+                "which": result["which"],
+                "ship_position": result["ship_position"],
+                "start": result["start"],
+                "left": result["left"],
+                "reclaimers_position": result["reclaimers_position"],
+                "obj": result["objective"],
+                "min_index": min_index,
+                "max_index": max_index
+            }
+
+            print("ship_indexes: ", [x for x in range(min_index, max_index+1)])
+            print("ship_size: ", self.ship_size[min_index:max_index+1])
+            print("which: ", result["which"])
+            print("start: ", [time + i * self.time_part for time in result["start"]])
+            print("left: ", [time + i * self.time_part for time in result["left"]])
+            print("obj: ", result["objective"])
+            print("min index: ", min_index)
+            print("max_index: ", max_index)
+
+            self.which.extend(result["which"])
+            self.ship_position.extend(result["ship_position"])
+            self.start.extend([time + i * self.time_part for time in result["start"]])
+            self.obj += result["objective"]
+
+            self.earlier_solution = dict_result
+
+    def set_initial_values(self, instance: Instance, part_number: int): 
+
+        min_idx, max_idx = self.find_range(part_number)
+
+        instance["nsh"] = max_idx - min_idx + 1
+        instance["ship_size"] = self.ship_size[min_idx:max_idx+1]
+        instance["load_goods"] = self.load_goods[min_idx:max_idx+1]
+        instance["maxtime"] = self.arrival[max_idx] - (self.time_part * part_number) + self.additional_time
+        instance["arrival"] = [time - (self.time_part * part_number) for time in self.arrival[min_idx:max_idx+1]]
         instance["ship_types_number"] = self.ship_types_number
         instance["ship_speed"] = self.ship_speed
         instance["ship_width"] = self.ship_width
@@ -54,14 +194,79 @@ class PartialAlgorithm:
         instance["reclaimers_number"] = self.reclaimers_number
         instance["reclaimers_platform"] = self.reclaimers_platform
 
-        max_time = timedelta(seconds=60)
-        result = instance.solve(timeout=max_time, processes=8)
-        
-        return {
-            "start": result["start"],
-            "which": result["which"],
-            "ship_position": result["ship_position"],
-            "obj": result["objective"]
-        }
+        print()
+        print("initial values")
 
+        print("nsh: ", max_idx - min_idx + 1)
+        print("ship size: ", self.ship_size[min_idx:max_idx+1])
+        print("load_goods: ", self.load_goods[min_idx:max_idx+1])
+        print("actual maxtime: ", self.arrival[max_idx] + self.additional_time)
+        print("set maxtime: ", self.arrival[max_idx] - (self.time_part * part_number) + self.additional_time)
+        print("actual arrival: ", [time for time in self.arrival[min_idx:max_idx+1]])
+        print("set arrival: ", [time - (self.time_part * part_number) for time in self.arrival[min_idx:max_idx+1]])
+
+        print()
+
+
+
+        return (min_idx, max_idx)
+
+    def set_earlier_values(self, instance: Instance, part_number: int): 
+
+        idx_range = self.find_earlier_range(self.earlier_solution['left'])
+        max_earlier_time = max(self.earlier_solution["left"]) - self.time_part
+
+        instance["pns"] = len(idx_range)
+        instance["max_earlier_time"] = max_earlier_time
+        instance["prev_ship_size"] = [self.ship_size[idx + self.earlier_solution["min_index"] + 1] for idx in idx_range]
+        instance["prev_which"] = [self.earlier_solution["which"][idx] for idx in idx_range]
+        instance["prev_ship_position"] = [self.earlier_solution["ship_position"][idx] for idx in idx_range]
+        instance["prev_start"] = [ max(self.earlier_solution["start"][idx] - self.time_part, 0) for idx in idx_range]
+        instance["prev_left"] = [self.earlier_solution["left"][idx] - self.time_part for idx in idx_range]
+
+        prev_reclaimers_position = []
+
+        for rec in self.earlier_solution["reclaimers_position"]:
+            prev_reclaimers_position.append(rec[self.time_part:max(self.earlier_solution["left"])])
+
+        instance["earlier_reclaimers_position"] = prev_reclaimers_position
+
+        print()
+        print("set earlier")
+        print("pns: ", len(idx_range))
+        print("max_earlier_time: ", max_earlier_time)
+        print("prev_ship_size: ", [self.ship_size[idx + self.earlier_solution["min_index"] + 1] for idx in idx_range])
+        print("prev_which: ", [self.earlier_solution["which"][idx] for idx in idx_range])
+        print("prev start: ",  [ max(self.earlier_solution["start"][idx] - self.time_part, 0) for idx in idx_range])
+        print("prev left: ", [self.earlier_solution["left"][idx] - self.time_part for idx in idx_range])
+        print()
+
+    def find_range(self, part_number):
+
+        min_time = self.time_part * part_number
+        max_time = self.time_part * (part_number + 1) - 1
+
+        min_index = float('inf')
+        max_index = -1
+
+        for idx, time in enumerate(self.arrival):
+            
+            if time >= min_time and idx < min_index:
+                min_index = idx
+            
+            if time <= max_time and idx > max_index:
+                max_index = idx
+
+        return (min_index, max_index)
     
+    def find_earlier_range(self, left_arr):
+
+        idx_range = []
+
+        for idx, time in enumerate(left_arr):
+            
+            if time > self.time_part:
+                idx_range.append(idx)
+
+        return idx_range
+
--- test.py
+++ test.py
@@ -1,16 +1,38 @@
 from solvers.dataGenerator.generator import Generator
 from solvers.partial import PartialAlgorithm
+import time
 
-number_of_ships = 6
-maxtime = 12
+number_of_ships = 100
+maxtime = 105
+additional_time = 14
+number_of_parts = 7
+stop_after = 20 # stop after 8 minutes
 
-generator = Generator(number_of_ships, maxtime)
+generator = Generator(number_of_ships, maxtime, additional_time)
 ship_size, arrival = generator.generate()
 
-partial = PartialAlgorithm(ship_size, arrival, maxtime, 5)
+# additional time must be less than (maxtime + 1) / number_of_parts
+
+start = time.time()
+
+partial = PartialAlgorithm( [1, 3, 1, 3, 2, 1, 3, 1, 1, 2, 1, 3, 1, 1, 3, 2, 3, 3, 1, 1, 2, 3, 2, 1, 2, 3, 3, 3, 1, 2, 2, 3, 1, 3, 1, 2, 1, 3, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 3, 3, 2, 1, 1, 3, 2, 2, 3, 2, 1, 1, 2, 2, 1, 2, 1, 1, 2, 2, 1, 1, 1, 3, 2, 1, 1, 2, 3, 1, 3, 3, 2, 1, 1, 1, 2, 1, 3, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 3, 1, 2],
+                            [1, 2, 2, 2, 4, 6, 7, 8, 9, 11, 11, 13, 13, 15, 16, 16, 16, 18, 18, 20, 21, 21, 22, 22, 24, 24, 25, 27, 29, 29, 29, 30, 30, 30, 32, 35, 35, 36, 36, 36, 37, 38, 38, 38, 39, 39, 40, 42, 42, 43, 43, 43, 44, 45, 45, 45, 45, 45, 47, 47, 48, 53, 53, 54, 55, 55, 57, 57, 57, 57, 58, 59, 61, 61, 64, 65, 66, 68, 69, 71, 71, 73, 73, 74, 74, 74, 75, 76, 77, 77, 78, 78, 79, 82, 82, 84, 85, 87, 88, 91],
+                            maxtime, number_of_parts, additional_time, stop_after
+                            )
 solution = partial.solve()
 
-print(solution)
+end = time.time()
 
+# [3, 1, 3, 3, 1, 1, 1, 3, 1, 3, 2, 2, 2, 3, 1, 1, 1, 2, 1, 1, 2, 1, 3, 2, 3, 1, 1, 2, 2, 1, 2, 2, 2, 1, 1, 3, 2, 2, 1, 2, 1, 2, 1, 2, 3, 3, 2, 1, 3, 3, 3, 2, 2, 2, 1],
+#                              [6, 8, 10, 22, 26, 29, 33, 39, 39, 40, 49, 51, 58, 60, 76, 84, 98, 103, 110, 110, 111, 115, 119, 121, 134, 138, 148, 149, 176, 177, 185, 185, 186, 188, 191, 192, 199, 202, 203, 212, 223, 225, 231, 234, 236, 241, 246, 251, 256, 259, 262, 265, 267, 273, 275],
+
+print("------------------------------------------- solution -------------------------------------------")
+print()
+print("which: ", solution["which"])
+print("ship position: ", solution["ship_position"])
+print("start time: ", solution["start"])
+print("obj: ", solution["obj"])
+print("time: ", f"{int((end - start) // 60)}min {int((end - start) % 60)}s")
+print()
 
 
